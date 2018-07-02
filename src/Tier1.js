e6d433e1e9759095441b61eef7b8333af0627f41
srajeev91 <github email address>
2018-07-02 09:57:53 -0400

    Done.

--- src/Tier1.js
+++ src/Tier1.js
@@ -18,9 +18,9 @@ export default class Tier1 extends Component {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div onClick={() => {this.setState({color: "#000"})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 color={"#0F0"} />
-        <Tier2 color={"#0FF"} />
+      <div onClick={() => {this.setState({color: getRandomColor()}, () => {this.setState({childColor: getReducedColor(this.state.color)})})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
+        <Tier2 color={"#0F0"} newColor={this.state.childColor}/>
+        <Tier2 color={"#0FF"} newColor={this.state.childColor}/>
       </div>
     )
   }
--- src/Tier2.js
+++ src/Tier2.js
@@ -8,17 +8,23 @@ export default class Tier2 extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      childColor: getReducedColor(this.props.color),
+      childColor: getReducedColor(this.props.newColor),
     }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      childColor: getReducedColor(nextProps.newColor)
+    })
   }
 
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
-        <Tier3 color={"#0F0"} />
-        <Tier3 color={"#F00"} />
+      <div className="tier2" style={{backgroundColor: this.props.newColor, color: this.props.newColor}}>
+        <Tier3 color={"#0F0"} newColor={this.state.childColor}/>
+        <Tier3 color={"#F00"} newColor={this.state.childColor}/>
       </div>
     )
   }
